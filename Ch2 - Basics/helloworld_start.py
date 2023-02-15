5f7d0f5ccd950b3e8b415540c00c81cdd9ad9302
Bailey Cage <bailey.cage@gmail.com>
2023-02-15 12:08:58 -0500

    complete Hello World exercise

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,12 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello, world!")
+    name = input("What is your name? ")
+    name = name.capitalize()
+    print(f"Nice to meet you, {name}!")
 
+# run main() if file executed as a program
+if __name__ == "__main__":
+    main()
