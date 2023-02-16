07287639d54b267ce1488274a8133e627f8920fb
Bailey Cage <bailey.cage@gmail.com>
2023-02-16 12:26:38 -0500

    complete modules exercise

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,12 +3,19 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
+import math
 
 
 # TODO: the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
 
 
 # TODO: in addition to functions, some modules contain useful constants 
+print("Pi is", math.pi)
 
 
 # TODO: try some of the math functions for yourself here:
+print("3.5 rounded up is", math.ceil(3.5))
+print("3.5 rounded down is", math.floor(3.5))
+print("The absolute value of -123 is", math.fabs(-123))
+print("2 to the power of 5 is", math.pow(2,5))
