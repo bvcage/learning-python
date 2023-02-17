6f5cbc7e0ffd9c2bf5dbec606a0c54b647f2f227
Bailey Cage <bailey.cage@gmail.com>
2023-02-17 13:55:42 -0500

    complete files exercise

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -5,19 +5,27 @@
 
 
 def main():  
-    # Open a file for writing and create it if it doesn't exist
-
+    # # Open a file for writing and create it if it doesn't exist
+    # my_file = open("textfile.txt", "w+")
     
-    # Open the file for appending text to the end
+    # # Open the file for appending text to the end
+    # my_file = open("textfile.txt", "a+")
 
-
-    # write some lines of data to the file
-
+    # # write some lines of data to the file
+    # for i in range(10):
+    #     my_file.write("This is some text\n")
     
-    # close the file when done
-
+    # # close the file when done
+    # my_file.close()
     
     # Open the file back up and read the contents
+    my_file = open("textfile.txt", "r")
+    if my_file.mode == "r":
+        # contents = my_file.read()
+        # print(contents)
+        file_lines = my_file.readlines()
+        for line in file_lines:
+            print(line)
 
     
 if __name__ == "__main__":
