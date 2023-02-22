1f199850f8e89613480eed3f39e24913c9668024
Bailey Cage <bailey.cage@gmail.com>
2023-02-22 11:26:34 -0500

    complete XML parsing exercise

--- Ch5 - Internet Data/xmlparsing_start.py
+++ Ch5 - Internet Data/xmlparsing_start.py
@@ -3,18 +3,32 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+import xml.dom.minidom
 
 def main():
     # use the parse() function to load and parse an XML file
-
+    doc = xml.dom.minidom.parse("samplexml.xml")
     
     # print out the document node and the name of the first child tag
-
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
 
     # get a list of XML tags from the document and print each one
-
+    skills = doc.getElementsByTagName("skill")
+    print(len(skills), "skills are listed:")
+    for s in skills:
+        print(s.getAttribute("name"))
       
     # create a new XML tag and add it into the document
+    new_skill = doc.createElement("skill")
+    new_skill.setAttribute("name", "jQuery")
+    doc.firstChild.appendChild(new_skill)
+
+    # print skills again to check new skill added
+    skills = doc.getElementsByTagName("skill")
+    print(len(skills), "skills are listed:")
+    for s in skills:
+        print(s.getAttribute("name"))
 
   
 
