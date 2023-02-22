859c5490ccbebacd7e336c3789ee57516e851161
Bailey Cage <bailey.cage@gmail.com>
2023-02-22 11:13:03 -0500

    complete JSON data exercise

--- Ch5 - Internet Data/jsondata_start.py
+++ Ch5 - Internet Data/jsondata_start.py
@@ -4,24 +4,40 @@
 #
 
 import urllib.request 
+import json
 
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
     
     # now we can access the contents of the JSON like any other Python object
-
+    if "title" in theJSON["metadata"]:
+        print(theJSON["metadata"]["title"])
     
     # output the number of events, plus the magnitude and each event name  
-
+    count = theJSON["metadata"]["count"]
+    print(count, "events recorded")
     
+    print("--------------\n")
     # for each event, print the place where it occurred
+    print("All quakes in the past day:")
+    for quake in theJSON["features"]:
+        print(quake["properties"]["place"])
 
-
+    print("--------------\n")
     # print the events that only have a magnitude greater than 4
+    print("Quakes with a magnitude of >= 4.0:")
+    for quake in theJSON["features"]:
+        if quake["properties"]["mag"] >= 4.0:
+            print(quake["properties"]["place"])
 
-
+    print("--------------\n")
     # print only the events where at least 1 person reported feeling something
+    print("Quakes where 1+ person reported feeling it:")
+    for quake in theJSON["features"]:
+        num_felt = quake["properties"]["felt"]
+        if num_felt != None and num_felt > 0:
+            print(quake["properties"]["place"])
 
   
 def main():
@@ -33,6 +49,12 @@ def main():
     # Open the URL and read the data
     webUrl = urllib.request.urlopen(urlData)
     print ("result code: " + str(webUrl.getcode()))
+
+    if webUrl.getcode() == 200:
+        data = webUrl.read()
+        printResults(data)
+    else:
+        print("Received error from server, cannot print results. Code:", webUrl.getCode())
   
 
 if __name__ == "__main__":
